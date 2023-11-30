0c92701c45e799ec67ab572ff32428d6d415b3b6
Santiego95 <103153028+Santiego95@users.noreply.github.com>
2023-11-30 19:11:26 -0300

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -11,3 +11,24 @@ document.querySelector('#relogio').innerHTML = `${hora}:${minuto}:${segundo}`
 mostraHora()
 setInterval(mostraHora, 1)
 
+let arrayCervejas = document.getElementById('arrayCervejas')
+
+function carregarArray (){
+    arrayCervejas.innerHTML = ""
+
+    for (let i = 0; i < dados.length; i++){
+        arrayCervejas.innerHTML += `
+        <div class="col-lg-3 col-md-4 col-6 mt-5 mb-5 pb-5">
+            <img src="${dados[i].src}" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">${dados[i].titulo}</h5>
+                <p class="card-text">${dados[i].descricao}</p>
+            </div> 
+        </div>
+            
+                    
+        `
+    }
+}
+
+carregarArray()
---
+++ js/array.js
@@ -0,0 +1,51 @@
+let dados = [
+    {
+        titulo: 'Pilsen',
+        descricao:'O estilo de cerveja artesanal Pilsen ou Pilsner surgiu na República Tcheca. Como características marcantes, a bebida apresenta aroma e sabor acentuados pelo lúpulo, além da cor dourada. Seu teor alcoólico varia entre 4,6% e 5% em média. As mais famosas são a cerveja de origem Pilsner Urquell (primeira Pilsen produzida) e a German Pilsner.',
+        valor:'10,00',
+        src:'../img/pilsen.jpg'
+    },
+    {
+        titulo: 'Lager',
+        descricao:'A cerveja Lager apresenta baixa fermentação, com ação do levedo em baixa temperatura. Trata-se do estilo favorito dos mestres cervejeiros alemães e tchecos, com graduação alcoólica que varia entre 4,7% e 5,5%. A Brooklyn Lager e a Bierland Vienna estão entre as cervejas mais conhecidas.',
+        valor:'10,00',
+        src:'../img/lager.jpg'
+    },
+    {
+        titulo: 'IPA',
+        descricao:'O estilo IPA ou India Pale Ale surgiu na Índia, com os soldados do exército britânico, que produziam cerveja com alta concentração de lúpulo. Por isso, esse estilo de cerveja apresenta sabor bem amargo, além do aroma frutado. Seu percentual alcoólico varia entre 5,5% e 7,5%. Os estilos IPA mais consumidos do mundo são a American Pale Ale e a Imperial IPA.',
+        valor:'10,00',
+        src:'../img/ipa.jpg'
+    },
+    {
+        titulo: 'Tripel',
+        descricao:'Criada na Bélgica, no Mosteiro Trapista de Westmalle, a cerveja Tripel apresenta cor clara, sabor amargo cítrico e aroma frutado. Esse estilo de cerveja artesanal é bem carbonatado, o que lhe confere uma espuma bastante cremosa. Trata-se de uma cerveja forte, com malte acentuado e teor alcoólico em torno de 7,5% e 8,5%.',
+        valor:'10,00',
+        src:'../img/tripel.jpg'
+    },
+    {
+        titulo: 'Porter',
+        descricao:'Criado na Inglaterra, o estilo Porter é um dos mais consumidos por ingleses e irlandeses. Sua cor escura vem da torra acentuada do malte, que também lhe proporciona aroma e sabor de café ou chocolate amargo. A cerveja Brown Porter apresenta menor teor alcoólico; já a Baltic Porter tem maior porcentagem de álcool.',
+        valor:'10,00',
+        src:'../img/porter.jpg'
+    },
+    {
+        titulo: 'Stout',
+        descricao:'A cerveja Stout é mais forte e escura que a Porter. Esse estilo apresenta aroma torrado e sabor amargo, que lembra café ou chocolate. Sua espuma é cremosa e pode ser clara ou amarronzada. A densidade, a coloração, o aroma, o sabor e o teor alcóolico podem variar conforme alguns fatores. Seu percentual de álcool chega a 8% em média.',
+        valor:'10,00',
+        src:'../img/stout.jpg'
+    },
+    {
+        titulo: 'Weizenbier',
+        descricao:'O estilo de cerveja Weizenbier, Weissbier ou Weiss surgiu na região Sul da Alemanha, mais especificamente na Baviera. O estilo apresenta 50% de malte de trigo (no mínimo). Sua cor é clara e opaca, com sabor e aroma frutados, lembrando banana e cravo. A bebida é refrescante, com teor alcóolico moderado (em torno de 5% a 6%).',
+        valor:'10,00',
+        src:'../img/bier.jpg'
+    },
+    {
+        titulo: 'Skol',
+        descricao:'Skol é uma cerveja clara, com aroma refinado e sabor leve e suave. Skol Pilsen é uma típica cerveja clara, com aroma refinado e sabor leve e suave. Seu teor de amargor é menos acentuado e seu teor alcoólico é médio.',
+        valor:'10,00',
+        src:'../img/skol.jpg'
+    },
+
+]
